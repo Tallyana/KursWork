4fdcf091cf62ad4f172b1b8a51375016b72aaac8
Admin <iggla67@yandex.ru>
2022-11-26 14:07:07 +0400

    3 задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Дан массив строк, необходимо удалить из него все дубли, оставшиеся строки объединить в одну в порядке следования в массиве.
+        String[] array = new String[] {"a", "a", "b", "a", "b", "a", "a", "b", "c", "a", "b", "a", "c", "a","d", "e", "d", };
+        System.out.println(deleteDuplicate(array));
+    }
+    public static String deleteDuplicate(String[] array) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null) {
+                for (int j = i + 1; j < array.length; j++) {
+                    if (array[i].equals(array[j])) {
+                        array[j] = null;
+                    }
+                }
+                stringBuilder.append(array[i]);
+            }
+        }
+        return stringBuilder.toString();
     }
 }
